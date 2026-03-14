f1a214cc4a6b61ee2f7800fe56f5edd92be94bfe
jayop99 <jaysonawane502@gmail.com>
2026-03-15 02:07:14 +0530

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,13 +1,16 @@
 import runpod
-from server import synthesize_speech
+from server import app
+from fastapi.testclient import TestClient
+
+client = TestClient(app)
 
 def handler(job):
     job_input = job["input"]
-    
+
     text = job_input.get("text", "")
-    
-    audio = synthesize_speech(text)
-    
-    return {"audio": audio}
+
+    response = client.get("/synthesize_speech/", params={"text": text})
+
+    return response.json()
 
 runpod.serverless.start({"handler": handler})
